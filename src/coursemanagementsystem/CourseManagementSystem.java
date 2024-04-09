5aeab651e6166e2eaae0d4f024499bf6ab523846
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-04-09 03:41:04 +0100

    Organised  the code

--- src/coursemanagementsystem/CourseManagementSystem.java
+++ src/coursemanagementsystem/CourseManagementSystem.java
@@ -10,30 +10,29 @@ import userFunctionalites.Authentication;
 import userFunctionalites.UserManagement;
 
 /**
- *
- * @Liudmila Grati 
+ * Main class for the Course Management System.
+ * @author Liudmila Grati
  */
 public class CourseManagementSystem {
 
     /**
+     * Main method, entry point of the application.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
-        // Create an instance of MyDatabaseIO
+        // Create an instance of MyDatabaseIO for database operations
         MyDatabaseIO dbIO = new MyDatabaseIO();
 
-        
-    // Initialise user Functionalities 
+        // Initialize user functionalities
         Authentication authentication = new Authentication(dbIO);
         UserManagement userService = new UserManagement(dbIO);
         
-        // Initialise ReportGenerator
+        // Initialize ReportGenerator for generating reports
         ReportGenerator reportGenerator = new ReportGenerator(dbIO);
         
-       
-        cmsMenu menu = new cmsMenu(authentication,userService,reportGenerator);
+        // Create a menu object to display the login menu
+        cmsMenu menu = new cmsMenu(authentication, userService, reportGenerator);
         menu.displayLoginMenu();
-        
-}
+    }
 }
