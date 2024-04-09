f1febcb7f804ba231a28bbc155ca947f3641c8dd
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-04-09 03:05:49 +0100

    Getters and Setters

--- src/UserFunctionalities/User.java
+++ src/UserFunctionalities/User.java
@@ -45,7 +45,80 @@ public class User {
         this.lecturerId = lecturerId;
         this.salt = salt;
     }
+// Getters and Setters
 
+    /**
+     * Gets the salt used for password hashing.
+     *
+     * @return The salt used for password hashing.
+     */
+    public String getSalt() {
+        return salt;
+    }
+
+    /**
+     * Sets the salt used for password hashing.
+     *
+     * @param salt The salt used for password hashing.
+     */
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    /**
+     * Gets the lecturer ID associated with the user.
+     *
+     * @return The lecturer ID associated with the user.
+     */
+    public String getLecturerId() {
+        return lecturerId;
+    }
+
+    /**
+     * Gets the username of the user.
+     *
+     * @return The username of the user.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Gets the role of the user.
+     *
+     * @return The role of the user.
+     */
+    public Role getRole() {
+        return role;
+    }
+
+    /**
+     * Gets the unique identifier of the user.
+     *
+     * @return The unique identifier of the user.
+     */
+    public String getUserID() {
+        return userID;
+    }
+
+    /**
+     * Sets the username of the user.
+     *
+     * @param username The username of the user.
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Sets the role of the user.
+     *
+     * @param role The role of the user.
+     */
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
     
     
 }
