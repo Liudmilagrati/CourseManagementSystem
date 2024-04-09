80a3c913d828bbe88b1efd877c313eec1e4241e6
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-04-09 02:52:49 +0100

    Refactored teh DB_IO class and added comments and Docstrings

--- src/DataBaseIntegration/DB_IO.java
+++ src/DataBaseIntegration/DB_IO.java
@@ -8,10 +8,26 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- *
- * @author user
+ * The DB_IO class provides methods for handling database connections.
  */
+
 public interface DB_IO {
+    
+    /**
+     * Retrieves a connection to the database.
+     * 
+     * @return a Connection object representing the database connection
+     * @throws SQLException if a database access error occurs
+     */
+    
     Connection getConnection() throws SQLException;
+    
+       /**
+     * Closes the database connection.
+     * 
+     * @param conn the Connection object representing the database connection to be closed
+     * @throws SQLException if a database access error occurs
+     */
+    
     public void closeConnection(Connection conn) throws SQLException;
 }
