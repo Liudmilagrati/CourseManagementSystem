0409bf3433b19c3783b6f9e9812c37280a624922
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-04-09 03:27:08 +0100

    Instance variables

--- src/Reports/ReportGenerator.java
+++ src/Reports/ReportGenerator.java
@@ -9,5 +9,10 @@ package Reports;
  * @author user
  */
 public class ReportGenerator {
+    // Instance variables
+    private DB_IO dbIO; // Database IO interface for database operations
+    private final ReportExporter reportExporter; // Object for exporting reports
+    private ReportGenerator reportGenerator;
+
     
 }
