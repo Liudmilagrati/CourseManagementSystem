535687c1a0f2f677a680a93a53009ec70b0147c6
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-04-09 03:11:23 +0100

    Update HashPassword

--- src/UserFunctionalities/HashPassword.java
+++ src/UserFunctionalities/HashPassword.java
@@ -4,10 +4,29 @@
  */
 package UserFunctionalities;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 /**
  *
  * @author user
  */
 public class HashPassword {
+    
+    public static String hashPassword(String plainPassword, String salt, int iterations) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = (plainPassword + salt).getBytes();
+            for (int i = 0; i < iterations; i++) {
+                md.update(hash);
+                hash = md.digest();
+            }
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
     
 }
