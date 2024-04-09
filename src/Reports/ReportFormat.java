23050d2870f9c5a4fdc23227f7ce86ab3a3b475c
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-04-09 03:24:38 +0100

    Created a new package Reports and a new java class ReportFormat

---
+++ src/Reports/ReportFormat.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reports;
+
+/**
+ *
+ * @Liudmila Grati 
+ */
+public enum ReportFormat {
+    TXT,
+    CSV,
+    CONSOLE
+}
