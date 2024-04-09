f0db0e5f59ac75e33d819bb89a874d40be2a1756
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-04-09 03:15:40 +0100

    Update UserManagement. Java class

--- src/UserFunctionalities/UserManagement.java
+++ src/UserFunctionalities/UserManagement.java
@@ -4,6 +4,7 @@
  */
 package UserFunctionalities;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,5 +22,21 @@ public class UserManagement {
         User admin = new User("admin", "java", Role.ADMIN);
         users.add(admin);
     }
+      public UserManagement(DB_IO dbIO) {
+        this.dbIO = dbIO;
+    }
+      
+      /**
+     * Allows updating or adding a user in the database.
+     *
+     * @param user          The User object containing user information.
+     * @param plainPassword Optional parameter if password is to be changed.
+     * @param salt          Salt string for the password.
+     * @return True if user was successfully updated or added, false otherwise.
+     */
+    public boolean updateUser(User user, String plainPassword, String salt) throws SQLException {
+        // Check if the user already exists in the database
+        boolean userExists = checkUserExists(user.getUsername());
+     
     
 }
