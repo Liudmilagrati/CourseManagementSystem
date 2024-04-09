15c2b21b775f6814ad3223dcf77011ad5e0a5743
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-04-09 02:58:24 +0100

    Refactored the class MyDatabaseIo and added comments and docstrings

--- src/DataBaseIntegration/MyDatabaseIO.java
+++ src/DataBaseIntegration/MyDatabaseIO.java
@@ -9,29 +9,45 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- *
- * @Liudmila Grati 
+ * MyDatabaseIO class provides methods to establish and close a database connection.
+ * It implements the DB_IO interface.
+ * 
+ * @author Liudmila Grati
  */
 public class MyDatabaseIO implements DB_IO {
 
-     // Instance variables to store database connection details
+    // Instance variables to store database connection details
     private String url;
     private String username;
     private String password;
 
-// Default constructor with database connection details
+    /**
+     * Default constructor with default database connection details.
+     * It connects to the MySQL database running on localhost using default credentials.
+     */
     public MyDatabaseIO() {
         this("jdbc:mysql://localhost/cms", "pooa2024", "pooa2024");
     }
     
-// Constructor with custom database connection details
+    /**
+     * Constructor with custom database connection details.
+     * 
+     * @param url      the URL of the database
+     * @param username the username for connecting to the database
+     * @param password the password for connecting to the database
+     */
     public MyDatabaseIO(String url, String username, String password) {
         this.url = url;
         this.username = username;
         this.password = password;
     }
     
-// Method to obtain a database connection
+    /**
+     * Obtains a database connection.
+     * 
+     * @return a Connection object representing the database connection
+     * @throws SQLException if a database access error occurs
+     */
     @Override
     public Connection getConnection() throws SQLException {
         // Establish a connection
@@ -42,7 +58,12 @@ public class MyDatabaseIO implements DB_IO {
         return conn;
     }
     
-     // Method to close a database connection
+    /**
+     * Closes a database connection.
+     * 
+     * @param conn the Connection object representing the database connection to be closed
+     * @throws SQLException if a database access error occurs
+     */
     @Override
     public void closeConnection(Connection conn) throws SQLException {
         if (conn != null) {
